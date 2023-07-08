c342c27041205814246cb16f1ec09cc94ede76e4
SDCOM-0415 <127208666+SDCOM-0415@users.noreply.github.com>
2023-07-08 13:58:29 +0800

    Upload by PicGo

---
+++ 什么奇行种.py
@@ -0,0 +1,28 @@
+import turtle
+import random
+
+__Pen = turtle.Pen()
+
+
+#开始进入Python的世界
+print('Hello')
+# 666
+for __count in range(4):
+    __Pen.forward(random.randint(1, 20))
+    __Pen.right(random.randint(1, 180))
+    __Pen.circle(random.randint(1, 50))
+__Pen.forward(random.randint(1, 20))
+for __count in range(4):
+    __Pen.forward(random.randint(1, 50))
+    __Pen.right(random.randint(1, 365))
+    __Pen.circle(random.randint(1, 50))
+__Pen.forward(random.randint(1, 50))
+for __count in range(4):
+    __Pen.forward(random.randint(1, 50))
+    __Pen.right(random.randint(1, 365))
+    __Pen.circle(random.randint(1, 50))
+__Pen.forward(random.randint(1, 50))
+for __count in range(4):
+    __Pen.forward(random.randint(1, 50))
+    __Pen.right(random.randint(1, 365))
+    __Pen.circle(random.randint(1, 50))

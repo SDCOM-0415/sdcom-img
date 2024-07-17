c36ae2b101b60d457274c4b53f825c404d887b73
SDCOM-0415 <127208666+SDCOM-0415@users.noreply.github.com>
2024-07-17 14:55:42 +0800

    Upload by PicGo

---
+++ start.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/SDCOM-0415/360Controller.git
+cd 360Controller
+bash ./build.sh
+# curl -sSL https://github.com/SDCOM-0415/360Controller/releases/download/1/start.sh | bash
